9888b9c13e1de8187a7aea6c65df36d1b47e9999
mmartins-vitor <mmartins.vitor@hotmail.com>
2022-07-07 17:38:43 -0300

    adição do metodo post, debbug

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+#request fornece a função form para passar dados do formulario para o servidor 
 #render_template trata-se de um Helper
 #importando framework
 
@@ -8,18 +9,34 @@ class Jogo():
         self.categoria = categoria
         self.console = console
         
+jogo1 = Jogo('Tetriz Z','Puzzle','Atari')
+jogo2 = Jogo('God Of War 2','Rack and Slash','PS2')
+jogo3 = Jogo('Mortal Kombat','Luta','PS2')
+lista = [jogo1, jogo2, jogo3]
+        
 
 app = Flask(__name__)
 # __name__ faz uma referencia ao próprio arquivo em questão 
 
 #para utilizar é necessário criar as rotas da web e uma função especificando sua ação
-@app.route('/init')
-def hello():
-    jogo1 = Jogo('Tetris','Puzzle','Atari')
-    jogo2 = Jogo('God Of War 2','Rack and Slash','PS2')
-    jogo3 = Jogo('Mortal Kombat','Luta','PS2')
-    lista = [jogo1, jogo2, jogo3]
+@app.route('/')
+def index():
     return render_template('lista.html', titulo = 'Jogos', jogos = lista)
 
+#criação da rota de cadastros de jogos
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods = ['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo = 'Jogos', jogos = lista)
 #para rodar a aplicação 
-app.run()
+app.run(
+    debug=True
+)
